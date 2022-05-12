1500f905dba5f6afaf6388ab0887f6d5feba79fc
BrickBrigade <101427661+BrickBrigade@users.noreply.github.com>
2022-05-12 17:24:28 -0500

    Update app.js

--- website/app.js
+++ website/app.js
@@ -1,6 +1,24 @@
-var express = require('express');
-var app = express();
+/* Function to POST data */
+const postData = async ( url = '', data = {})=>{
+  console.log(data)
+    const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header        
+  });
 
-app.get('/', function (req, res) {
-	res.send("'hello world'");
-})
+    try {
+      const newData = await response.json();
+      // console.log(newData);
+      return newData
+    }catch(error) {
+    console.log("error", error);
+    // appropriately handle the error
+    }
+}
+
+// TODO-Call Function
+postData('/addAnimal', {animal:'cat'})
